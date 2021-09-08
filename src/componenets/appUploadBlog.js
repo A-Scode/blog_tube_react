372e76001c1ad2aea20c4f84d4b3f8f6a9327ecc
shouryaraj <shouryarajsinghgoud@gmail.com>
2021-09-08 07:49:00 +0530

    blog info form skeleton

--- src/componenets/appUploadBlog.js
+++ src/componenets/appUploadBlog.js
@@ -1,4 +1,4 @@
-import {useEffect ,useContext} from 'react'
+import {useEffect ,useContext , useState} from 'react'
 import { Redirect ,useHistory } from 'react-router'
 import { Login_context } from '../App'
 
@@ -12,15 +12,22 @@ var AppUploadBlog = props=>{
             return null
         }        
     },[])
+
+    const [blog_details , set_blog_details] = useState({})
     
     const blog_info = (
         <div className="blog_info">
             <form action="javascript:void(0)"></form>
             <label htmlFor="blog_title">Title</label>
-            <input type="text" placeholder = "Title" id = "blog_title" maxLength={100} />
+            <input type="text" placeholder = "Title" id = "blog_title" maxLength={100} required />
 
             <label htmlFor="blog_discription">Discription</label>
             <textarea type="text"  id="blog_discription" placeholder="Discription" maxLength={200} />
+
+            <label htmlFor="blog_image">image</label>
+            <input type="file" id = "blog_image" />
+
+            <input type="submit" value="Next" />
 
         </div>
     )
